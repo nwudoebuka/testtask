107f1da7664f3b7acbe5dbbb0aac855721781d52
Johnbosco Ibemere <j.ibemere@dreammesh.local>
2018-11-27 21:19:45 +0100

    aded

--- app/src/main/java/com/example/ebuka/testtask/room/tables/Employee.java
+++ app/src/main/java/com/example/ebuka/testtask/room/tables/Employee.java
@@ -1,15 +1,20 @@
 package com.example.ebuka.testtask.room.tables;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
 @Entity(tableName = "employee")
 public class Employee {
-   // private String name;
-   // @PrimaryKey @NonNull
-   // private String phoneNumber;
+    @PrimaryKey(autoGenerate = true)
+    private long id;
 
-   // @PrimaryKey(autoGenerate = true)
-   // private long id;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
--- app/src/main/java/com/example/ebuka/testtask/room/tables/Employer.java
+++ app/src/main/java/com/example/ebuka/testtask/room/tables/Employer.java
@@ -1,16 +1,23 @@
 package com.example.ebuka.testtask.room.tables;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 
 @Entity(tableName = "employer")
 public class Employer {
    // private String name;
    // @PrimaryKey @NonNull
    // private String phoneNumber;
-
-   // @PrimaryKey(autoGenerate = true)
-   // private long id;
-
     //private Date date;
 
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
